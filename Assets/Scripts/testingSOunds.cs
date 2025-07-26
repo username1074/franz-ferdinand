61d4f33d2e61463b04e1bbe22ebacc2a735dfeb3
lara-j-ds <laradonaldsonscott@gmail.com>
2025-07-26 16:21:22 +1200

    Sound Manager (#5)
    * Added sounds
    * Added start up sound
    * sound manager

---
+++ Assets/Scripts/testingSOunds.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingSOunds : MonoBehaviour
+{
+    public AudioClip music;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundManager.Instance.PlayMusic(music);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
