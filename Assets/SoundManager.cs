358c78aae9ef9110d62370f80396cc72eeffff87
lara-j-ds <laradonaldsonscott@gmail.com>
2025-07-26 15:16:05 +1200

    sound stuff

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// mainly used the learn.nity sound manager tutorial and "Building a Sound Manager" from Brian Moakley @ Jezner
 public class SoundManager : MonoBehaviour
 {
-    //Allows other scripts to call functions from the SoundManager
+
+    //Allows other scripts to call functions from the SoundManager--> singleton
     public static SoundManager instance = null;
 
     // Audio players components.
@@ -20,6 +22,9 @@ public class SoundManager : MonoBehaviour
     [field: SerializeField] public AudioClip mistake { get; set; }
 
     [field: SerializeField] public AudioClip failure { get; set; }
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
 
     // Start is called before the first frame update
     void Start()
@@ -37,12 +42,30 @@ public class SoundManager : MonoBehaviour
         EffectsSource.clip = clip;
         EffectsSource.Play();
     }
-    
+
     // Play a single clip through the music source.
-	public void PlayMusic(AudioClip clip)
-	{
-		MusicSource.clip = clip;
-		MusicSource.Play();
-	}
+    public void PlayMusic(AudioClip clip)
+    {
+        MusicSource.clip = clip;
+        MusicSource.Play();
+
+        // to use in a component/script:
+        // public AudioClip <MusicClip>; as datafield
+        // SoundManager.Instance.PlayMusic(<MusicClip>);
+    }
+
+    //RandomizeSfx chooses randomly between various audio clips and slightly changes their pitch.
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+        EffectsSource.pitch = randomPitch;
+        EffectsSource.clip = clips[randomIndex];
+        EffectsSource.Play();
+
+        // to use in component/script:
+        // public AudioClip[] <RandomNoises>; as datafield
+        // SoundManager.Instance.RandomSoundEffect(<RandomNoises>);
+    }
 
 }
