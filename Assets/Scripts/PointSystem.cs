d3e3c3e3d9a5904ea09fa6b357be01993a4b91f2
Jenny de la Harpe <jennydelaharpe@Jennys-MacBook-Pro.local>
2025-07-27 13:29:57 +1200

    more stuff

--- Assets/Scripts/PointSystem.cs
+++ Assets/Scripts/PointSystem.cs
@@ -8,13 +8,16 @@ public class PointSystem : MonoBehaviour
 {
     public int lives;
     public GameObject end;
+    public Image flashObject;  
+    public float fadeDuration = 1.0f;
+    public float visibleDuration = 0.5f;
 
-    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
         lives = 3;
         end.SetActive(false);
+        flashObject.enabled = false;
     }
     void Update()
     {
@@ -27,8 +30,35 @@ public class PointSystem : MonoBehaviour
     public void Ouch()
     {
         lives--;
-        slider.value = lives;
-        
+        if (lives != 0)
+        {
+            StartCoroutine(FlashCoroutine());
+        }
+
+    }
+
+
+    private IEnumerator FlashCoroutine()
+    {
+        flashObject.enabled = true;
+        Color color = flashObject.color;
+        color.a = 0.3f;
+        flashObject.color = color;
+
+        yield return new WaitForSeconds(visibleDuration);
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Lerp(0.3f, 0f, elapsed / fadeDuration);
+            flashObject.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        flashObject.color = color;
+        flashObject.enabled = false;
     }
 
 }
